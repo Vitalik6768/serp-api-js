423718a68dcf1d2efd4a8597c474ff2a515814d1
vitalik <vitalgreg1988@gmail.com>
2022-04-10 20:21:58 +0300

    dfdf

--- js/app.js
+++ js/app.js
@@ -1,19 +1,16 @@
 
-const apiKey = '';
+const apiKey = 'fdcf0d55319161f5e027b459910763a0';
 let serpData = [];
 let newData = [];
-let webSites = [];
+let competitorSites = [];
 let keyword;
-let con = 0;
+let keyWordCount = 0;
 
 const btn = document.querySelector('#button-addon2');
 const textFeald = document.querySelector('#comment');
 const urlFeald = document.querySelector('#urlFeald');
 const spinner = document.querySelector('#spin');
 spinner.style.display = "none";
-
-
-
 
 
 btn.addEventListener('click', function() {
@@ -27,7 +24,7 @@ btn.addEventListener('click', function() {
   }else{
     alert('Please Enter Keyword');
   }
-
+  
 });
 
 makeRequest = (searchKeyword, lin) =>{
@@ -36,11 +33,11 @@ makeRequest = (searchKeyword, lin) =>{
           serpData.push(response.data);
           spinner.style.display = "none";
           keyword = searchKeyword;
-          con++;
+          keyWordCount++;
           console.log('request');
-          displsyData();
-          if(lin == con){
-            similarWeb();
+          filterData();
+          if(lin == keyWordCount){
+            findCompetitors();
           }
       })
       .catch(function (error) {
@@ -48,7 +45,7 @@ makeRequest = (searchKeyword, lin) =>{
       })
 }
 
-let displsyData = (siteRank) => {
+let filterData = (siteRank) => {
   let maxPosition = 0;
   for(let site of serpData){
     let org = site.organic_results;
@@ -59,14 +56,13 @@ let displsyData = (siteRank) => {
            const newJsaon = new Object;
            newJsaon.rank = elm.position;
            newJsaon.url = elm.domain;
-          // console.log(`function key ${keyword}`);
            newJsaon.keyword = keyword;
            newData.push(newJsaon);
-           webSites.push(elm.domain);
+           competitorSites.push(elm.domain);
            serpData = [];
          }
        }else{
-         sortData(elm);
+         findWebsite(elm);
        }
      }
   }
--- js/sort.js
+++ js/sort.js
@@ -1,4 +1,5 @@
-const sortData = (elm, siteRank) => {
+const findWebsite = (elm, siteRank) => {
+
    if(elm.domain == urlFeald.value){
      header = document.querySelector('#header');
      header.innerText = urlFeald.value;
@@ -8,40 +9,44 @@ const sortData = (elm, siteRank) => {
      Rank: ${elm.position}
      `;
      let siteRank = elm.position;
-     displsyData(siteRank);
+     filterData(siteRank);
    }
 }
 
-
-const similarWeb = () =>{
+const findCompetitors = () =>{
   const counts = {};
-  webSites.forEach((x) => {
+  competitorSites.forEach((x) => {
     counts[x] = (counts[x] || 0) + 1;
       });
   for (let key in counts) {
       if(counts[key] > 1){
-        let li = document.createElement('li');
-        li.className = "list-group-item d-flex justify-content-between align-items-start";
-        let divA = document.createElement('div');
-        divA.className = "ms-2 me-auto";
-        let divB = document.createElement('div');
-        divB.className = "fw-bold";
-        divB.innerText = `${key}`;
-        let divC = document.createElement('div');
-        // li.appendChild(badge);
-        li.appendChild(divA);
-        divA.appendChild(divB);
-        divA.insertBefore(divC, null);
-
-        for(let site of newData){
-          if(site.url == key){
-            let p = document.createElement('p');
-            p.innerText=` Key Word: ${site.keyword} | Rank: ${site.rank} `;
-            divC.appendChild(p);
-            // badge.innerText = `3`;
-          }
-          document.querySelector('ol').appendChild(li);
+        createList(key);
       }
     }
+  }
+
+
+
+const createList = (key) =>{
+  let li = document.createElement('li');
+  li.className = "list-group-item d-flex justify-content-between align-items-start";
+  let divA = document.createElement('div');
+  divA.className = "ms-2 me-auto";
+  let divB = document.createElement('div');
+  divB.className = "fw-bold";
+  divB.innerText = `${key}`;
+  let divC = document.createElement('div');
+  // li.appendChild(badge);
+  li.appendChild(divA);
+  divA.appendChild(divB);
+  divA.insertBefore(divC, null);
+
+  for(let site of newData){
+    if(site.url == key){
+      let p = document.createElement('p');
+      p.innerText=` Key Word: ${site.keyword} | Rank: ${site.rank} `;
+      divC.appendChild(p);
+    }
+    document.querySelector('ol').appendChild(li);
   }
 }
